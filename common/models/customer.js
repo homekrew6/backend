d446832062221bee35683ccb2b450986c43f5b59
Anup Chakraborty <nits.anup@gmail.com>
2017-11-13 18:18:30 +0530

    customer edit

--- common/models/customer.js
+++ common/models/customer.js
@@ -1,5 +1,22 @@
 'use strict';
 
 module.exports = function(Customer) {
-
+  Customer.editCustomer = function(id,customer,cb){
+    customer.id = id;
+    Customer.upsert( customer, function (err, res) {
+        cb(null, res);
+    });
+  }
+  Customer.remoteMethod('editCustomer', {
+          http: {path: '/editCustomer/:id', verb: 'put'},
+           accepts: [
+            {arg: 'id', type: 'number',required:true},
+            {
+              arg: 'customer',
+              type: 'object',
+              http:{source:'body'}
+            }
+          ],
+            returns: {arg: 'customer', type: 'object'}
+    });
 };
