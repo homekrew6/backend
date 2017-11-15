12e033f12af3014ff3dc27e90446e278618f110a
Anup Chakraborty <nits.anup@gmail.com>
2017-11-15 18:56:34 +0530

    worker edit

--- common/models/worker.js
+++ common/models/worker.js
@@ -1,5 +1,22 @@
 'use strict';
 
 module.exports = function(Worker) {
-
+  Worker.editWorker = function(id,worker,cb){
+    worker.id = id;
+    Worker.upsert( worker, function (err, res) {
+        cb(null, res);
+    });
+  }
+  Worker.remoteMethod('editWorker', {
+          http: {path: '/editWorker/:id', verb: 'put'},
+           accepts: [
+            {arg: 'id', type: 'number',required:true},
+            {
+              arg: 'worker',
+              type: 'object',
+              http:{source:'body'}
+            }
+          ],
+            returns: {arg: 'worker', type: 'object'}
+    });
 };
